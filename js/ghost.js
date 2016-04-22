ecacca579584131afa7d4160663c8feb8331657f
Andreas Elia <andreaselia@live.co.uk>
2016-04-22 15:09:35 +0100

    Added three.js library and initial file structure

---
+++ js/ghost.js
@@ -0,0 +1,14 @@
+/**
+ * Ghost class for handling all of each ghost's updating etc.
+ */
+var Ghost = function() {
+    this.speed = 4;
+
+    this.init = function() {
+
+    };
+
+    this.update = function() {
+
+    };
+};
---
+++ js/pacman.js
@@ -0,0 +1,14 @@
+/**
+ * Pacman class for handling all of pacman's updating etc.
+ */
+var Pacman = function() {
+    this.speed = 4;
+
+    this.init = function() {
+
+    };
+
+    this.update = function() {
+
+    };
+};
